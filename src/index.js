855e8d5cbd6baaed3920878dd5bb66fa4042c08b
Dmitrii Barilkin <dimabaril@gmail.com>
2023-08-10 03:27:31 +0300

    works in console but something wrong on test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+function towelSort(arr) {
+    for (i = 0; i < arr.length; i++) {
+        if (i % 2) {
+            arr[i].reverse();
+        }
+    }
+    return arr.flat();
 }
+
+module.exports = function towelSort(matrix) {
+    return [];
+};
+
+// const matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ];
+// // result [ 1, 2, 3, 6, 5, 4, 7, 8, 9 ]
+// console.log(towelSort(matrix));
